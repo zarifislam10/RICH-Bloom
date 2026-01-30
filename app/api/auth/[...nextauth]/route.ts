045f80693fa2d649a815d28639b3440534e9399d
Shakil Nabil <shakilnabil11@gmail.com>
2026-01-30 17:16:10 -0500

    implemented google sign in

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+})
+
+export { handler as GET, handler as POST }
